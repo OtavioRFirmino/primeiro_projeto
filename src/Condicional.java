faee4fb6c2035d2c24ec1aa3183c91a180fe8b2f
OtavioRFirmino <tavinho.renato@gmail.com>
2025-05-27 21:16:21 -0300

    Aula 3.4

--- src/Condicional.java
+++ src/Condicional.java
@@ -3,11 +3,18 @@ public class Condicional {
         int anoDeLancamento = 2023;
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
+        String tipoPlano = "plus";
 
         if (anoDeLancamento >= 2023){
             System.out.println("Lançamento que está bombando!");
         } else {
             System.out.println("Filme retrô!");
+        }
+
+        if (incluidoNoPlano == true || tipoPlano.equals("plus")){
+            System.out.println("Filme liberado!");
+            } else {
+            System.out.println("Deve pagar a locação!");
         }
     }
 }
---
+++ src/Leitura.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Leitura {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite seu filme favorito");
+        String filme = leitura.nextLine();
+        System.out.println("Qual o ano de lançamento?");
+        int anoLancamento = leitura.nextInt();
+        System.out.println("Sua nota");
+        double avaliacao = leitura.nextDouble();
+
+        System.out.println(filme);
+        System.out.println(anoLancamento);
+        System.out.println(avaliacao);
+    }
+}
